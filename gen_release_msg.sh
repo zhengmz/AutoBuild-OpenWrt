dc823b8715633ae199528c62d0f1f5d41952e74f
zhengmz-docker <zhengmz@flowsea.com>
2022-05-09 09:40:27 +0800

    update gen_release.msg.sh: add upstream link

--- gen_release_msg.sh
+++ gen_release_msg.sh
@@ -8,28 +8,36 @@ cat >$file <<EOF
 ### 文件说明
 
 1. 本固件为自动编译
-2. *.bin 为自动生成的固件文件
-3. *sdk*.xz 为自动生成的 SDK 文件 (如果编译的是 SDK)
-4. *.7z 和 *.zip 为已编译通过的模块包
-   - 其中 targets/**/sha256sums 文件有固件和 SDK 文件的校验值
+1. \`*.bin\` 为自动生成的固件文件
+1. \`*.7z\` 和 \`*.zip\` 为已编译通过的模块包
+   - 其中 \`targets/**/sha256sums\` 文件有固件和 SDK 文件的校验值
 
 ### 默认参数
 
-- 默认IP： 192.168.10.1
-- 默认密码： password
+- 默认IP： \`192.168.10.1\`
+- 默认密码： \`password\`
 
 ---
 
 ### 使用到的仓库及其版本
 
 EOF
+
+[ "x$BUILD_SDK" = "xtrue" ] && sed -i '7i\1. \`*sdk*.xz\` 为自动生成的 SDK 文件' $file
+
 for d in `find $srcdir -type d -name ".git"`
 do
 	(
 	cd $d/..
 	name=$(basename $PWD); [[ "$name" == "lede" ]] && name="openwrt/lede"
-        ver_date=$(git show -s --date=short --format="commit: %H, date: %ci")
-	echo "- <b>$name</b>: $ver_date" >> $file
+	upstream=$(git remote show -n origin|grep "Fetch URL"|awk -F': ' '{print $2}')
+	upstream=${upstream%.git}
+        #ver_date=$(git show -s --date=short --format="commit: %H, date: %ci")
+	#echo "- <b>$name</b>: $ver_date" >> $file
+        commit=$(git show -s --date=short --format="%H")
+        date=$(git show -s --date=short --format="%ci")
+	#echo "- <b>$name</b>: commit:$commit, date:$date" >> $file
+	echo "- [$name]($upstream): commit:[$commit]($upstream/tree/$commit), date:$date" >> $file
 	)
 done
 for d in `find $srcdir -type d -name ".svn"`
@@ -37,9 +45,11 @@ do
 	(
 	cd $d/..
 	name=$(basename $PWD)
+	upstream=$(svn info --show-item repos-root-url)
 	rev=$(svn info --show-item last-changed-revision)
 	date=$(svn info --show-item last-changed-date)
-	echo "- <b>$name</b>: rev:$rev, date:$date" >> $file
+	#echo "- <b>$name</b>: rev:$rev, date:$date" >> $file
+	echo "- [$name]($upstream): rev:$rev, date:$date" >> $file
 	)
 done
 
